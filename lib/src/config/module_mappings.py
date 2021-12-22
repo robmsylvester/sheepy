c74b139377cf4e2a0a5f15b35fa8da393e7db554
robmsylvester <robmsylvester@gmail.com>
2021-12-22 13:21:29 -0800

    chore: remove unused augmented transformer files

--- lib/src/config/module_mappings.py
+++ lib/src/config/module_mappings.py
@@ -6,9 +6,7 @@ from lib.src.data_modules.examples.toxic_comment_data_module import ToxicComment
 from lib.src.data_modules.examples.tweet_sentiment_data_module import TweetSentimentDataModule
 from lib.src.data_modules.examples.semeval_sentiment_data_module import SemEvalSentimentDataModule
 
-from lib.src.models.multilabel_augmented_transformer_classifier import MultiLabelAugmentedTransformerClassifier
 from lib.src.models.base_transformer_classifier import TransformerClassifier
-from lib.src.models.augmented_transformer_classifier import AugmentedTransformerClassifier
 from lib.src.models.multiclass_transformer_classifier import MulticlassTransformerClassifier
 from lib.src.models.multilabel_transformer_classifier import MultiLabelTransformerClassifier
 
@@ -23,8 +21,6 @@ data_module_mapping = {
 }
 
 model_mapping = {
-    'augmented_transformer_classifier': AugmentedTransformerClassifier,
-    'multilabel_augmented_transformer_classifier': MultiLabelAugmentedTransformerClassifier,
     'transformer_classifier': TransformerClassifier,
     'multiclass_transformer_classifier': MulticlassTransformerClassifier,
     'multilabel_transformer_classifier': MultiLabelTransformerClassifier
