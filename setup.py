10888fe79e1852444a4b367e61b794b80d4625f2
Christfried Focke <christfried.focke@appfolio.com>
2022-05-15 22:48:21 +0000

    Move version declaration into __init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from setuptools import setup, find_packages
 import os
+import re
 import sys
-sys.path.append(os.path.dirname(__file__))
 
-__version__ = "0.1.4"
+from setuptools import find_packages, setup
+
+sys.path.append(os.path.dirname(__file__))
+PACKAGE_NAME = "sheepy"
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+PACKAGE_INIT_FILE = os.path.join(CURRENT_DIR, PACKAGE_NAME, "__init__.py")
+with open(os.path.join(CURRENT_DIR, "README.md"), encoding="utf-8") as fp:
+    README = fp.read()
+with open(PACKAGE_INIT_FILE, encoding="utf-8") as fp:
+    VERSION = re.search('__version__ = "([^"]+)"', fp.read()).group(1)
 
 setup(
-    name='sheepy',
-    version=__version__,
+    name=PACKAGE_NAME,
+    version=VERSION,
+    long_description=README,
     description='Experiment-tracking wrapper around PyTorch Lightning, Weights & Biases, Pandas, and Huggingface Trasformers that aims at rapid experimentation for common text classification tasks using transformers',
     author='Rob Sylvester',
     author_email='robmsylvester@gmail.com',
