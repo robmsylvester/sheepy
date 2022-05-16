5d852ed1e4779091a53e1e0168d790545f14f97d
Christfried Focke <christfried.focke@appfolio.com>
2022-05-15 22:00:59 -0700

    Move data module mappings into main.py

--- examples/main.py
+++ examples/main.py
@@ -2,12 +2,23 @@ import argparse
 import json
 
 import torch
-from module_mappings import DATA_MODULE_MAPPING
+from semeval_sentiment_data_module import SemEvalSentimentDataModule
+from sms_spam_data_module import SmsSpamDataModule
+from toxic_comment_data_module import ToxicCommentDataModule
 
+from sheepy.data_modules.base_csv_data_module import BaseCSVDataModule
+from sheepy.data_modules.multi_label_csv_data_module import MultiLabelCSVDataModule
 from sheepy.experiment.base_experiment import Experiment
 from sheepy.experiment.sweep_experiment import SweepExperiment
 from sheepy.models import MODEL_MAPPING
 
+DATA_MODULE_MAPPING = {
+    'base_csv_data_module': BaseCSVDataModule,
+    'toxic_comment_data_module': MultiLabelCSVDataModule,
+    'sms_spam_data_module': SmsSpamDataModule,
+    'toxic_comment_data_module': ToxicCommentDataModule,
+    'semeval_data_module': SemEvalSentimentDataModule,
+}
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
