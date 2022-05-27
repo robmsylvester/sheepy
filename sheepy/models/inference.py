9cce38e6042f4745820201b1955fab23d4ac4f4f
Christfried Focke <christfried.focke@appfolio.com>
2022-05-27 23:23:42 +0000

    Fix inference script

--- sheepy/models/inference.py
+++ sheepy/models/inference.py
@@ -3,9 +3,7 @@ import os
 
 import pandas as pd
 import torch
-from transformers import AutoModel, AutoTokenizer
-
-from sheepy.models.fully_connected_classifier import FullyConnectedClassifier
+from transformers import AutoModel, AutoModelForSequenceClassification, AutoTokenizer
 
 
 class SequenceClassificationModelRunner:
@@ -13,16 +11,13 @@ class SequenceClassificationModelRunner:
         with open(os.path.join(model_dir, "config.json")) as f:
             self.config = json.load(f)
 
-        self.model = AutoModel.from_pretrained(model_dir)
-        self.classification_head = FullyConnectedClassifier(self.config["layer_config"])
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_dir)
         self.model.eval()
-
         self.tokenizer = AutoTokenizer.from_pretrained(model_dir)
 
     def predict(self, text, min_prob=0.0):
         inputs = self.tokenizer(text, return_tensors="pt")
-        word_embeddings = self.model(**inputs)[0]
-        logits = self.classification_head(word_embeddings)
+        logits = self.model(**inputs)[0]
         probs = torch.sigmoid(logits)
         probs = probs.detach().cpu().numpy()
 
